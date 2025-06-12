ef496864e3e93f38e93c762519703dcd74babc75
Lucas Meireles <98045972+Katuner@users.noreply.github.com>
2025-06-12 16:54:59 -0300

    Inicio dos testes

---
+++ src/test/java/CadastroRendimentoTeste.java
@@ -0,0 +1,21 @@
+import org.example.IRPF;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CadastroRendimentoTeste {
+
+    private IRPF irpf;
+
+    @Before
+    public void setUp(){
+      irpf = new IRPF();
+    }
+
+    @Test
+    public void testCadastrarSalario(){
+        fail("Não implementado");
+    }
+
+}
